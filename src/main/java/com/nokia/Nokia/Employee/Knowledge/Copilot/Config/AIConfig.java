53dafaebb1e2bb9d70d223d298107ce31243ec2b
mdswaley <mdswaley075@gmail.com>
2026-07-04 16:01:48 +0530

    Adding dependency, configuration and properties

---
+++ src/main/java/com/nokia/Nokia/Employee/Knowledge/Copilot/Config/AIConfig.java
@@ -0,0 +1,19 @@
+package com.nokia.Nokia.Employee.Knowledge.Copilot.Config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AIConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder){
+        return builder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+
+}
