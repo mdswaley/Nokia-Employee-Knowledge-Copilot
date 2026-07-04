1eac3464dd9d9b5f393e8f9ebc014d4b1a9c4a5a
mdswaley <mdswaley075@gmail.com>
2026-07-04 15:44:21 +0530

    first commit

---
+++ src/main/java/com/nokia/Nokia/Employee/Knowledge/Copilot/NokiaEmployeeKnowledgeCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.Nokia.Employee.Knowledge.Copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NokiaEmployeeKnowledgeCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NokiaEmployeeKnowledgeCopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nokia/Nokia/Employee/Knowledge/Copilot/NokiaEmployeeKnowledgeCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokia.Nokia.Employee.Knowledge.Copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NokiaEmployeeKnowledgeCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
